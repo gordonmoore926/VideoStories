c61a81fd9fc8edfcb6ca0acff4347eef19fc9a7f
Gordon Moore <gordonmoore@nat-oitwireless-inside-vapornet100-a-13475.princeton.edu>
2017-10-18 13:05:00 -0400

    Initial Commit

---
+++ VideoStories/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VideoStories
+//
+//  Created by Gordon Moore on 10/18/17.
+//  Copyright © 2017 GMAL. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
