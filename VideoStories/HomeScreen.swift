c5e73d980cd0973b22d98300985ba002dacc5d35
Gordon Moore <gordonmoore@nat-oitwireless-inside-vapornet100-a-13475.Princeton.EDU>
2017-10-25 00:27:05 -0400

    Login Front End GM

---
+++ VideoStories/HomeScreen.swift
@@ -0,0 +1,47 @@
+//
+//  LoginScreen.swift
+//  VideoStories
+//
+//  Created by Gordon Moore on 10/24/17.
+//  Copyright © 2017 GMAL. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func MoreWaysToLoginButton(_ sender: Any) {
+        performSegue(withIdentifier: "HomeToMoreWays", sender: self)
+    }
+    
+    @IBAction func EmailLogin(_ sender: Any) {
+        performSegue(withIdentifier: "HomeToEmailLogin", sender: self)
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
